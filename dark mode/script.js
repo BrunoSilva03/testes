9c4fbcb18de0eaeea20b10aad157f300767d3a9c
BrunoSilva03 <bruno5ssilva02@gmail.com>
2023-05-24 12:23:36 -0300

    Deu +/- certo

--- dark mode/script.js
+++ dark mode/script.js
@@ -1,22 +1,32 @@
-var button = document.getElementById('botao');
+//var button = document.getElementById('botao');
 
-button.addEventListener('click', modoDiamodoNoite);
+//button.addEventListener('click', modoDiamodoNoite);
 
-let noite = false;
+//let noite = false;
 
 function modoDiamodoNoite() {
+    alert('Funcionou');
     
-    let title = document.querySelectorAll('h1');
-    let divs = document.querySelectorAll('div');
+    let titulo = document.querySelector('h1');
+    let corpo = document.querySelector('body');
+    let asdivs = document.querySelectorAll('div');
+    let artigos = document.querySelector('.artigos');
     let sessao = document.getElementById('sessaobotao');
+    let botao = document.getElementById('botao');
+    let art2 = document.getElementById('artigo2');
 
 
     
-        title.style.backgroundColor = '#000';
-        divs.style.backgroundColor = '#000'
-        sessao.style.backgroundColor = '#000';
+        //titulo.style.backgroundColor = '#000';
+        titulo.style.color = 'white';
+        corpo.style.backgroundColor = 'black';
+        artigos.style.color = '#fff';
+        asdivs.style.color = 'blue';
+        sessao.style.backgroundColor = '#fff';
+        botao.style.backgroundColor = 'white';
+        art2.style.color = 'white';
 
-        alert('Funcionou')
+        alert('Funcionou2')
 
     /*
     for(let i = 0; i < asDivs.length; i += 1) {
