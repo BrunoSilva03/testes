1c2608c539a6d2b04b1cc228f06bb2847fd3d77a
BrunoSilva03 <bruno5ssilva02@gmail.com>
2023-06-02 14:04:29 -0300

    eventos de mouse jquery

---
+++ jquery3 eventos de mouse/script.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+
+    var ex = $('.ex1');
+
+    $('.ev1').click(function() {
+        $(this).css("background","#ccc");
+        ex.text("Você Clicou!");
+    });
+
+    $('.ev2').dblclick(function() {
+        $(this).css("background", "#ccc");
+        ex.text("Você deu Dois Cliques");
+    })
+
+    
+    $('.ev3').focusin(function() {
+        $(this).css("background", "#ccc");
+        ex.text("Você deu foco no input");
+    });
+
+
+    $('.ev3').focusout(function() {
+        $(this).css("background", "#000");
+        ex.text("Você tirou o foco do input");
+    })
+    
+
+    //OU
+
+    /*
+    $('.ev3').focusin(function() {
+        $(this).css("background", "#ccc");
+        ex.text("Você deu foco no input");
+    }).focusout(function() {
+        $(this).css("background", "#000");
+        ex.text("Você deu foco no input");
+    })
+    */
+
+    
+});
