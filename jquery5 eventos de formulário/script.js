08f4599c509a117028fb1a42b6fd57c9fc0af4e3
BrunoSilva03 <bruno5ssilva02@gmail.com>
2023-07-10 14:01:49 -0300

    vish

---
+++ jquery5 eventos de formulário/script.js
@@ -0,0 +1,28 @@
+$(function() {
+
+    $('.place').each(function() {
+        var place = $(this).attr('title');
+        var input = $(this);
+
+        input
+            .val(place)
+            .css('color','#ccc')
+            .focusin(function() {
+                input.css('color','#000');
+                if(input.val() == place) {
+                    input.val('');
+                }
+            }).focusout(function() {
+                if(input.val() == '') {
+                    input.css('color','#ccc');
+                    input.val(place);
+                }
+                
+                
+            })
+    });
+
+
+    var ex = $('.ex1');
+    
+});
