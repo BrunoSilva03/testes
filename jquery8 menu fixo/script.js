c5a0a0b99abd364257a787c05e12e1e9f981adb8
BrunoSilva03 <bruno5ssilva02@gmail.com>
2023-07-12 13:42:27 -0300

    menu que rola junto com o usuário

--- jquery8 menu fixo/script.js
+++ jquery8 menu fixo/script.js
@@ -1,3 +1,14 @@
 $(function() {
-    
+
+    var nav = $('.menu');
+
+    $(window).scroll(function() {
+
+        if($(this).scrollTop() > 64.71) {
+            nav.addClass("menu-fixo")
+        } else {
+            nav.removeClass("menu-fixo")
+        }
+
+    })
 })
