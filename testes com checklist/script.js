70b6431e3fa212023309f0c6175f368b40c511c1
BrunoSilva03 <bruno5ssilva02@gmail.com>
2023-08-02 10:41:15 -0300

    Falta alguns ajustes no update

--- testes com checklist/script.js
+++ testes com checklist/script.js
@@ -1,9 +1,14 @@
 $(document).ready(function() {
 
-    $('input').change(function() {
+    var valorUpdate;
+    var contador = 1;
+    var idTarefa;
+
+    $('.container input').change(function() {
         var input = $(this).val();
-        $('ul').append('<li>' + input + ' <i class="fas fa-check"></i> <i class="fas fa-trash"></i></li>');
+        $('ul').append(`<li id="${contador}">` + input + ' <i class="fas fa-check"></i> <i class="fas fa-trash"></i> <i class="fas fa-pen"></i></li>');
         $(this).val('');
+        contador++;
     });
 
     $('ul').on('click', '.fa-check', function() {
@@ -12,6 +17,42 @@ $(document).ready(function() {
 
     $('ul').on('click', '.fa-trash', function() {
         $(this).parent('li').fadeOut(300);
+    });
+
+    $('ul').on('click', '.fa-pen', function() {
+        $('.container').hide();
+        $('.formupdate').fadeIn();
+        idTarefa = $(this).parent('li').attr('id');
+
+       
+       
+    });
+
+    $('#btncancelar').on('click', function() {
+        $('#inputupdate').val('');
+        $('.formupdate').hide();
+        $('.container').fadeIn();
+    });
+
+
+    $('#btnconfirma').on('click', function() {
+        valorUpdate = $('#inputupdate').val();
+        $('ul').each(function() {
+
+            if($('li').attr('id').val() == idTarefa) {
+                alert('achou');
+            }
+        });
+        
+        $('.formupdate').hide();
+        $('.container').fadeIn();
+        $('#inputupdate').val('');
+        alert(valorUpdate);
     })
+    
+    
+    
+    
+
 
 });
